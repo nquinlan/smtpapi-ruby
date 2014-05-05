3e230396ba8d99d91761e1183f908d3fccbd0052
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-05-05 15:04:57 -0700

    correct returns

--- lib/smtpapi.rb
+++ lib/smtpapi.rb
@@ -11,54 +11,67 @@ class Header
         "section"     => {},
         "filters"     => {},
     }
+    return self
   end
 
   def add_to(email, name = nil)
     @data['to'] << (name ? name + " <" + email + ">" : email)
+    return self
   end
 
   def set_tos(emails)
     @data['to'] = Array(emails) rescue nil
+    return self
   end
 
   def add_substitution(from_value, to_values)
     @data['sub'][from_value] = Array(to_values) rescue nil
+    return self
   end
 
   def set_substitutions(substitutions)
     @data['sub'] = substitutions
+    return self
   end
 
   def add_unique_arg(key, value)
     @data['unique_args'][key] = value
+    return self
   end
 
   def set_unique_args(args)
     @data['unique_args'] = args
+    return self
   end
 
   def add_category(category)
     @data['category'] << category
+    return self
   end
 
   def set_categories(categories)
     @data['category'] = categories
+    return self
   end
 
   def add_section(from_value, to_value)
     @data['section'][from_value] = to_values
+    return self
   end
 
   def set_section(sections)
     @data['section'] = sections
+    return self
   end
 
   def add_filter(filter, setting, value)
     @data['filters'][filter]['settings'][setting] = value
+    return self
   end
 
   def set_filters(filters)
     @data['filters'] = filters
+    return self
   end
 
   def json_string
